147d7622783d25ff703a16ecbdbf17429e4508a4
cambridgejames <cambridge_james@foxmail.com>
2019-07-21 17:53:19 +0800

    1. 修复了公式编辑控制器无法识别事件来源的问题

--- js/include/equation_editor.js
+++ js/include/equation_editor.js
@@ -3,9 +3,14 @@
  */
 function EquationControler() {
 	// 公式编辑控制器
+	this.object;
+	this.index;
+
 	this.getEquation = function() {
+		// 先清空输入框和预览内容，再显示模态框
+		$("#equationModal").find("textarea[class*=form-control]").val("");
+		$("#equationModal").find(".modal-body>div>span").html("");
 		$("#equationModal").modal("show");
-		return "    ";
 	}
 }
 var equationCtrl = new EquationControler();
@@ -14,13 +19,18 @@ $(document).on("click", ".input-group-addon", function(event) {
 	// 功能：单击插入公式按钮时触发按钮编辑器
 	var targInput = $(event.target).prev();
 	var index = targInput.getCurPos();
-	var content = targInput.val();
-	var equation = equationCtrl.getEquation();
-	if(equation.length === 0) {
-		return;
-	}
-	content = content.substr(0, index) + "$$" + equation + "$$" + content.substr(index, content.length);
-	targInput.val(content);
-	targInput.setCurPos(index + equation.length + 4, index + equation.length + 4);
+	equationCtrl.object = targInput;
+	equationCtrl.index = index;
+	equationCtrl.getEquation();
+});
+
+$(document).on("click", "#equationModal button[class*=btn-primary][data-dismiss]", function() {
+	var equation = $("#equationModal").find("textarea[class*=form-control]").val();
+	if(equation.length === 0) { return; }
+	equation = "$$" + equation + "$$";
+	var content = equationCtrl.object.val();
+	content = content.substr(0, equationCtrl.index) + equation + content.substr(equationCtrl.index, content.length);
+	equationCtrl.object.val(content);
+	equationCtrl.object.setCurPos(equationCtrl.index + equation.length, equationCtrl.index + equation.length);
 });
 
