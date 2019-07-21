8a2113fb5287bc85121b3ffc506e57ba1a9d074a
cambridgejames <cambridge_james@foxmail.com>
2019-07-21 23:41:20 +0800

    1. 完成了试卷作答页面的基本结构 2. 添加了试卷作答页面作答和显示、隐藏试题列表的事件响应 3. 修复了题目表格控制器的事件响应冲突

--- js/include/paper-answering-view.js
+++
@@ -1,8 +0,0 @@
-$(document).on("shown.bs.collapse", "#question-list-panel", function() {
-	$("#question-list-btn").children(":first-child").html("隐藏题目列表");
-});
-
-$(document).on("hidden.bs.collapse", "#question-list-panel", function() {
-	$("#question-list-btn").children(":first-child").html("展开题目列表");
-});
-
