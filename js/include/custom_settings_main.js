ca1c3ef5c206f850d2e9d2c3ca3bacae5c84cee0
cambridgejames <cambridge_james@foxmail.com>
2019-07-30 17:43:22 +0800

    1. 修复了新密码中无法识别非法特殊符号的问题 2. 优化了新密码校验逻辑

--- js/include/custom_settings_main.js
+++ js/include/custom_settings_main.js
@@ -35,26 +35,30 @@ $(document).on("input propertychange", "li[data-setting=change-password]>div[dat
 	}
 });
 
-$(document).on("click", "li[data-setting=change-password]>div[data-effect=submit]>button", function() {
-	var firstInput = $("li[data-setting=change-password]>div[data-effect=new] input[type=password]");
-	var secondInput = $("li[data-setting=change-password]>div[data-effect=repeat] input[type=password]");
-	var tester = /[\u4e00-\u9fa5]+|[" "]/g;
-	if(firstInput.val() !== secondInput.val()) {
+$(document).on("click", "li[data-setting=change-password]>div[data-effect=submit]>a", function() {
+	var firstInputVal = $("li[data-setting=change-password]>div[data-effect=new] input[type=password]").val();
+	var secondInputVal = $("li[data-setting=change-password]>div[data-effect=repeat] input[type=password]").val();
+	if(firstInputVal !== secondInputVal) {
 		$(this).prev().html("两次输入的新密码不一致");
 		return;
-	} else if(firstInput.val().length < 6 || firstInput.val().length > 50) {
+	}
+	if(firstInputVal.length < 6 || firstInputVal.length > 50) {
 		$(this).prev().html("新密码长度不符合规范");
 		return;
 	}
-	var matcher = firstInput.val().match(tester);
-	var input = firstInput.val();
-	for(var index = 0; index < matcher.length; index++) {
-		input = input.replace(matcher[index], "");
+	var tester = /[A-Z]|[a-z]|[0-9]|[!,%,&,@,#,$,^,*,?,_,~]/g;
+	var matcher = firstInputVal.match(tester);
+	var input = firstInputVal;
+	if(matcher) {
+		for(var index = 0; index < matcher.length; index++) {
+			input = input.replace(matcher[index], "");
+		}
 	}
-	if(input.length !== 0) {
+	if(!matcher || input.length !== 0) {
 		$(this).prev().html("新密码包含非法字符");
 		return;
 	}
 	// TODO: 密码上传逻辑
+	$(this).prev().html("");
 });
 
